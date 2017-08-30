06893d4dfd54498167613c1db5fcfabd12fc3035
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-08-29 23:22:03 -0400

    built out home and about me

--- app/client/components/About.js
+++ app/client/components/About.js
@@ -9,7 +9,17 @@ class About extends React.Component {
 
 	render() {
 		return (
-			<div>About</div>
+			<div id='about'>
+				<h2>Lorem Ipsum</h2>
+				<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Ted_Geisel_NYWTS_2_crop.jpg/451px-Ted_Geisel_NYWTS_2_crop.jpg'/>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum risus massa, non facilisis metus dapibus in. Maecenas iaculis semper tincidunt. Donec tincidunt molestie odio. Phasellus et magna at ipsum varius faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Nunc sed imperdiet diam. Duis nec mauris a ex mattis gravida at vel neque. Ut id laoreet nisi. Duis ut convallis tellus, vel efficitur metus.</p>
+				<p>Donec et tristique nunc. Vestibulum non leo gravida, maximus tellus eu, aliquam magna. Curabitur sed finibus nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ante condimentum, eleifend erat at, tristique elit. Suspendisse non ex ex. Curabitur est mi, vestibulum sit amet est sit amet, vehicula efficitur nisl. Phasellus vitae nunc sodales, dictum dui vel, pulvinar turpis. Phasellus nec fringilla justo.</p>
+				<hr/>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum risus massa, non facilisis metus dapibus in. Maecenas iaculis semper tincidunt. Donec tincidunt molestie odio. Phasellus et magna at ipsum varius faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Nunc sed imperdiet diam. Duis nec mauris a ex mattis gravida at vel neque. Ut id laoreet nisi. Duis ut convallis tellus, vel efficitur metus.</p>
+				<p>Donec et tristique nunc. Vestibulum non leo gravida, maximus tellus eu, aliquam magna. Curabitur sed finibus nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet ante condimentum, eleifend erat at, tristique elit. Suspendisse non ex ex. Curabitur est mi, vestibulum sit amet est sit amet, vehicula efficitur nisl. Phasellus vitae nunc sodales, dictum dui vel, pulvinar turpis. Phasellus nec fringilla justo.</p>
+			</div>
 		);
 	}
 }
--- app/client/components/Home.js
+++ app/client/components/Home.js
@@ -9,7 +9,13 @@ class Home extends React.Component {
 
 	render() {
 		return (
-			<div>Home</div>
+			<div id='home'>
+				<h2>Lorem Ipsum</h2>
+				<img src='https://vignette4.wikia.nocookie.net/seuss/images/d/db/Dr-seuss-cat-in-the-hat-clip-art-free-clipart.jpeg/revision/latest?cb=20170126062846'/>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum risus massa, non facilisis metus dapibus in. Maecenas iaculis semper tincidunt. Donec tincidunt molestie odio.</p>
+				<p>Phasellus et magna at ipsum varius faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Nunc sed imperdiet diam. Duis nec mauris a ex mattis gravida at vel neque.</p>
+				<p>Ut id laoreet nisi. Duis ut convallis tellus, vel efficitur metus.</p>
+			</div>
 		);
 	}
 }
