dcb467f12edb8fc70db10d587fb006b16e2e255c
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-15 21:03:56 -0400

    added a modal for books

--- app/client/components/App.js
+++ app/client/components/App.js
@@ -15,8 +15,6 @@ class Header extends React.Component {
 	}
 
 	render() {
-		console.log(this.props.children)
-
 		return (
 			<div id="app">
 				<header>
--- app/client/components/Books.js
+++ app/client/components/Books.js
@@ -2,14 +2,67 @@
 
 import React from 'react';
 
+import Modal from './Modal';
+
 class Books extends React.Component {
 	constructor(props) {
 		super(props)
+		
+		this.state = { modal: false };
+	}
+
+	click() {
+		this.setState({ modal: !this.state.modal });
 	}
 
 	render() {
 		return (
-			<div>Books</div>
+			<div>
+				<h2>Click a book to learn more!</h2>
+
+				<div id='books'>
+					{ !this.state.modal ? null : <Modal close={this.click.bind(this)}/> }
+
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='ruler'></div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='ruler'></div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+					<div className='book-container' onClick={this.click.bind(this)}>
+						<img src='https://images-na.ssl-images-amazon.com/images/I/51N595qwKOL._SX360_BO1,204,203,200_.jpg'/>
+						<p>Lorem Ipsum</p>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
