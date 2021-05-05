fc2bb421a00411e075dd79715ae591ae371858d1
Nicholas Zeiss <nick.zeiss@hireart.com>
2021-05-05 19:00:30 -0400

    update to about me section

--- app/client/components/About.js
+++ app/client/components/About.js
@@ -3,25 +3,49 @@
 import React from 'react';
 
 
+// const About = props => {
+// 	return (
+// 		<section id='about'>
+// 			<img src='client/images/blue.jpg' />
+// 			<p>
+// 				Greetings! I am Gloria Koster, a children's author and librarian. For many year's I worked full-time at a wonderful elementary school.
+// 				Now I am lucky enough to be the part-time director of the <a href='http://www.poundridgelibrary.org/'>Pound Ridge Library</a> in my hometown of Pound Ridge, New York. Working part-time
+// 				provides more opportunity for writing, and that's fantastic! I am working on a humorous picture book and finishing up two picture book
+// 				biographies. Stay tuned.
+// 			</p>
+// 			<p>
+// 				My husband Eric and I have three adult children and a batch of little grandsons. When we're not at home, we're generally heading north to
+// 				Vermont or into nearby New York City. We also love to travel, especially to France, where I continue to embarrass myself by stumbling
+// 				over the elegant language. Quel dommage!
+// 			</p>
+// 			<p>
+// 	 			My newest book, <strong><i>Little Red Ruthie</i></strong>, was published in August of 2017. Like my previous book, <strong><i>Mitzi's Mitzvah</i></strong>, it is a <a href='https://pjlibrary.org/home'>
+// 	 			PJ Library selection.</a> My first book, <strong><i>The Peanut-Free Caf&eacute;</i></strong>, was inspired by all the time I spent in a school lunchroom, watching a lot of fussy eaters
+// 	 			and the challenges faced by kids with food allergies.
+// 	 		</p>
+//  		</section>
+// 	);
+// }
+
+
 const About = props => {
 	return (
 		<section id='about'>
 			<img src='client/images/blue.jpg' />
 			<p>
-				Greetings! I am Gloria Koster, a children's author and librarian. For many year's I worked full-time at a wonderful elementary school.
-				Now I am lucky enough to be the part-time director of the <a href='http://www.poundridgelibrary.org/'>Pound Ridge Library</a> in my hometown of Pound Ridge, New York. Working part-time
-				provides more opportunity for writing, and that's fantastic! I am working on a humorous picture book and finishing up two picture book
-				biographies. Stay tuned.
+				Greetings! I am Gloria Koster, a children's author and librarian. For many years I worked full-time at a wonderful Connecticut elementary school. I’ve also served as an interim director of <a target="_blank" href="http://www.poundridgelibrary.org/">Pound Ridge Library</a> in my hometown of Pound Ridge, New York.
 			</p>
 			<p>
-				My husband Eric and I have three adult children and a batch of little grandsons. When we're not at home, we're generally heading north to
-				Vermont or into nearby New York City. We also love to travel, especially to France, where I continue to embarrass myself by stumbling
-				over the elegant language. Quel dommage!
+				Currently I am a member of the Children's Book Committee of the Bank Street College of Education. Each year we read hundreds of new titles to select the very finest choices to be included in our <a target="_blank" href="https://www.bankstreet.edu/library/center-for-childrens-literature/childrens-book-committee/best-childrens-books-of-the-year/">Best Books guide.</a>
 			</p>
 			<p>
-	 			My newest book, <strong><i>Little Red Ruthie</i></strong>, was published in August of 2017. Like my previous book, <strong><i>Mitzi's Mitzvah</i></strong>, it is a <a href='https://pjlibrary.org/home'>
-	 			PJ Library selection.</a> My first book, <strong><i>The Peanut-Free Caf&eacute;</i></strong>, was inspired by all the time I spent in a school lunchroom, watching a lot of fussy eaters
-	 			and the challenges faced by kids with food allergies.
+				I’m also a member of the Women’s National Book Association, serving as Co-Chair of the WNBA Pannell Award. Each year we honor two Independent Book Stores for their services to children.
+	 		</p>
+	 		<p>
+	 			My husband Eric and I have three adult children and four grandkids. When we're not at home, we're generally heading north to Vermont or into nearby New York City. We also love to travel, especially to France, where I continue to embarrass myself by stumbling over the elegant language. Quel dommage!
+	 		</p>
+	 		<p>
+	 			My first book, The Peanut-Free Café, was inspired by all the time I spent in a school lunchroom, watching a lot of fussy eaters and the challenges faced by kids with food allergies. My other titles include Little Red Ruthie and Mitzi's Mitzvah. Each of these is a <a target="_blank" href="https://pjlibrary.org/home">PJ Library selection.</a>  And stay tuned for my newest book! Dance the Hora, Isadora! will be published in 2022 by <a target="_blank" href="https://store.behrmanhouse.com/index.php/apples-and-honey-press.html">Apples & Honey Press.</a> 
 	 		</p>
  		</section>
 	);
